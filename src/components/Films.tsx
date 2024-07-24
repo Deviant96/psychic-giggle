50e05fbbdb5e02ef6ae51a8b5f72c69c693ecb7f
Deviant96 <mcrealdeviant@gmail.com>
2024-07-24 17:18:35 +0700

    add mobile design for film list

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -18,23 +18,23 @@ const Films: React.FC = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-20 px-10">
+    <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 py-10 px-4 md:gap-4 md:py-20 md:px-10">
       {films.length < 1 ? (
         <FilmListSkeleton />
       ) : (
         <>
           {films.map((film, index) => (
-            <div key={film.episode_id} className="min-h-[200px]">
+            <div key={film.episode_id} className="min-h-[150px] md:min-h-[200px]">
               <div className="w-full relative h-full">
                 <img
                   src={`https://picsum.photos/200/100?random=${Math.random()}`}
                   alt={`Movie poster`}
                   className="object-cover h-full w-full rounded-lg"
                 />
-                <div className="z-20 absolute bottom-0 left-0 z-10 -translate-y-2/4 ms-4 flex flex-row gap-2">
-                  <h2 className="text-xl font-bold">{film.title}</h2>
+                <div className="z-20 absolute bottom-0 left-0 z-10 ms-2 mb-2 md:-translate-y-2/4 md:ms-4 flex flex-col md:flex-row gap-2">
+                  <h2 className="text-lg md:text-xl font-bold order-2 md:order-1">{film.title}</h2>
                   {index === 2 && (
-                    <span className="bg-yellow-500 rounded px-2 py-1 text-sm text-black font-bold">Exclusive</span>
+                    <span className="bg-yellow-500 rounded px-2 py-1 text-sm text-black font-bold order-1 md:order-2 w-fit"> Exclusive </span>
                   )}
                 </div>
                 <div className="w-full h-full absolute top-0 left-0 bg-dark-fade-in-bottom"></div>
