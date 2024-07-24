ddd72692ad09d4292e93821d4e1a81c41bed98ea
deviant96 <mcrealdeviant@gmail.com>
2024-07-24 23:47:30 +0700

    Fix request typing

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest } from 'next/server';
 
-export async function POST(req: NextResponse, res: NextRequest) {
+export async function POST(req: NextRequest) {
   const { username, password } = await req.json();
 
   if (username === 'user' && password === 'password') {
