629a9c991aaf1a8277a41a57d6cdef907de73949
Deviant96 <mcrealdeviant@gmail.com>
2024-07-24 16:40:48 +0700

    Redesign movie list

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -17,11 +17,23 @@ const Films: React.FC = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {films.map((film) => (
-        <div key={film.episode_id} className="p-4 border border-gray-300">
-          <h2 className="text-xl font-bold">{film.title}</h2>
-          <p>{film.opening_crawl}</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-20 px-10">
+      {films.map((film, index) => (
+        <div key={film.episode_id} className="min-h-[200px]">
+          <div className="w-full relative h-full">
+            <img
+              src={`https://picsum.photos/200/100?random=${Math.random()}`}
+              alt={`Movie poster`}
+              className="object-cover h-full w-full rounded-lg"
+            />
+            <div className="z-20 absolute bottom-0 left-0 z-10 -translate-y-2/4 ms-4 flex flex-row gap-2">
+              <h2 className="text-xl font-bold">{film.title}</h2>
+              {index === 2 && (
+                <span className="bg-yellow-500 rounded px-2 py-1 text-sm text-black font-bold">Exclusive</span>
+              )}
+            </div>
+            <div className="w-full h-full absolute top-0 left-0 bg-dark-fade-in-bottom"></div>
+          </div>
         </div>
       ))}
     </div>
